e3ac08a339556bdc168cbf49c8f3fb5ec1405e33
swapnildalvi90 <sdalvi@tavisca.com>
2014-09-05 08:29:47 +0530

    validation for new user

--- HotDog-master/GroupOn/js/validation.js
+++ HotDog-master/GroupOn/js/validation.js
@@ -25,3 +25,36 @@ var flag=true;
  }
  return flag;
 }
+
+function uservalidation()
+{
+ var full_name=document.getElementById("full_name").value;
+ var email = document.getElementById("email_id").value;
+ var password  = document.getElementById("pswd").value;
+ var conf_password=document.getElementById("confirm_pswd").value;
+var email_flag=true;
+var password_flag=true;
+
+ var emailpattern=/^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
+ var passwordpattern=/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+ if(password!=conf_password){
+ 	password_flag=false;
+ }  
+
+ if( !emailpattern.test(email) ){
+  email_flag=false;
+  document.getElementById("email_failed").innerHTML="Enter valid Email Id";
+ }
+ else{
+ 	document.getElementById("email_failed").innerHTML="";
+ }
+
+ if(!passwordpattern.test(password)){
+  flag=false;
+  document.getElementById("pswd_failed").innerHTML="Enter valid password";
+ }
+ else{
+ 	document.getElementById("pswd_failed").innerHTML="";
+ }
+ return flag;	
+}
