1f791fffd9d755b743ce88fa920617880736f567
swapnildalvi90 <sdalvi@tavisca.com>
2014-09-11 17:36:47 +0530

    ajax, xml, jquery functionality

--- HotDog-master/GroupOn/js/thumbload.js
+++ HotDog-master/GroupOn/js/thumbload.js
@@ -1,25 +1,34 @@
 $(document).ready(function(){
-  alert(jsonobj.start[0].category);
+  
+  $.ajax({
+                    url: "json/deal.json",
+                    //force to handle it as text
+                    dataType: "text",
+                    success: function(data) {
+                        
+                        var json = $.parseJSON(data);
+                        
+                        for(i=0;i<4;i++)
+                        {
+                        str=json.start[i].photo;
+                        $("#"+i+"_thumbnail").html("<img width='100%' height='100%' src='"+str+"'/>");
 
-  for(i=0;i<4;i++)
-  {
-  str=jsonobj.start[i].photo;
-  $("#"+i+"_thumbnail").html("<img width='100%' height='100%' src='"+str+"'/>");
+                        str_title=json.start[i]._title;
+                        $("#"+i+"_title").html(str_title);
 
-  str_title=jsonobj.start[i]._title;
-  $("#"+i+"_title").html(str_title);
+                        str_desc=json.start[i]._location;
+                        $("#"+i+"_desc").html(str_desc);
 
-  str_desc=jsonobj.start[i]._location;
-  $("#"+i+"_desc").html(str_desc);
+                        str_loc=json.start[i].city;
+                        $("#"+i+"_locn").html(str_loc);
 
-  str_loc=jsonobj.start[i].city;
-  $("#"+i+"_locn").html(str_loc);
+                        str_ogcost=json.start[i].original_value;
+                        $("#"+i+"_price").html("$"+str_ogcost+"&nbsp");
 
-  str_ogcost=jsonobj.start[i].original_value;
-  $("#"+i+"_price").html("$"+str_ogcost+"&nbsp");
-
-  str_discount=jsonobj.start[i].discount_price;
-  $("#"+i+"_disc").html("From $"+str_discount+"&nbsp");
+                        str_discount=json.start[i].discount_price;
+                        $("#"+i+"_disc").html("From $"+str_discount+"&nbsp");
  
-  }
+                        }
+                     }
+                });
 });
